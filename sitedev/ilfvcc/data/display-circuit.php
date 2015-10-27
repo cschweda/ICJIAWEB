b6c451156cf7c552307aba0db47cabc5d63633f7
cschweda <cschweda@gmail.com>
2015-10-27 09:11:32 -0500

    dam viol update

---
+++ sitedev/ilfvcc/data/display-circuit.php
@@ -0,0 +1,45 @@
+
+<!-- <?php
+    echo $_GET['c'];
+?> -->
+
+<style>
+.rule {padding-bottom: 5px; border-bottom: 1px solid #ddd}
+h2.h5 {font-weight: 700}
+</style>
+
+
+<h2 class="h5 rule">Counties Served</h2>
+<p>
+Christian, Clay, Clinton, Effingham, Fayette, Jasper, Marion, Montgomery, Shelby</p>
+
+<h2 class="h5 rule">Council Chairs</h2>
+<ul>
+<li>Hon. James Eder - Chair</li>
+<li>Hon. Ericka Sanders - Vice-Chair</li>
+<li>Brenda Benton, Local Council Coordinator, 4th Judicial Circuit FVCC</li>
+</ul>
+<h2 class="h5 rule">Coordinator</h2>
+<p>
+<strong>Brenda Benton, Local Council Coordinator</strong><br/>4th Judicial Circuit FVCC<br/>
+27390 W. 4th St. Road<br/>
+ 				Centralia, IL 62801<br/>
+ 				(618) 292-6477<br/>
+(618) 533-1530<br/>
+<a href="##">brenda.benton56@gmail.com</a>
+</p>
+
+<h2 class="h5 rule">Committees</h2>
+<ul>
+<li>
+				Law Enforcement/OVW Committee</li>
+			<li>	Helping Services Committee</li>
+    </ul>
+
+<h2 class="h5 rule">Publications</h2>
+http://www.ilfvcc.org/local/04/4th%20Circuit%20FVCC%20Fact%20Sheet.pdf
+4th Circuit FVCC brochure
+
+<h2 class="h5 rule">Website</h2>
+					Facebook:  https://www.facebook.com/4thJudicialCircuitFVCC?fref=ts
+</div>
