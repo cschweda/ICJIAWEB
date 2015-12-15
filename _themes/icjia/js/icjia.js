bd8fa659da561f83faee2313bad579072b112af1
cschweda <cschweda@gmail.com>
2015-12-15 09:54:14 -0600

    changed times for labels

--- _themes/icjia/js/icjia.js
+++ _themes/icjia/js/icjia.js
@@ -643,9 +643,9 @@ $(document).ready(function() {
 
 
 	/* Add 'new' label to front page news items and articles. Added 12/01/2015. Uses moment.js date library  */
-	var daysToShowNewLabelforNews = 5;
-	var daysToShowUpdatedLabelforNews = 5;
-	var daysToShowNewLabelforArticles = 15;
+	var daysToShowNewLabelforNews = 3;
+	var daysToShowUpdatedLabelforNews = 3;
+	var daysToShowNewLabelforArticles = 10;
 	var newLabel =
 		'<span style="font-weight: 700; background: #fe8869; color: #fff; border: 0px solid #ccc; padding: 4px; font-family: Lato, sans-serif">NEW</span>&nbsp;&nbsp;';
 
