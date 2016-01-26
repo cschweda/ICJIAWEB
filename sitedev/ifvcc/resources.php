8caa8cb27810eea5f81bf1de9c7f4c6b13b1d29a
cschweda <cschweda@gmail.com>
2016-01-26 15:10:38 -0600

    ifvcc resources updated

--- sitedev/ifvcc/resources.php
+++ sitedev/ifvcc/resources.php
@@ -86,7 +86,7 @@
 
 <h2 class="h4 resource-heading" style="">Elder Abuse</h2>
 
-<ul>
+<ul class="ifvcc-resources-list">
 	<li>National Center on Elder Abuse
 	<br><a href="http://www.ncea.aoa.gov">www.ncea.aoa.gov</a> </li>
 	<li>Illinois Department on Aging <br>
@@ -96,7 +96,7 @@
 
 <h2 class="h4 resource-heading" style="">People with Disabilities</h2>
 
-<ul>
+<ul class="ifvcc-resources-list">
 	<li>Illinois Images
 	<br><a href="http://Illinoisimages.org" target="_blank">Illinoisimages.org</a> </li>
 	<li>National Center on Criminal Justice and Disability
@@ -104,6 +104,68 @@
 	<li>National Disability Rights Network
 	<br><a href="http://www.ndrn.org">www.ndrn.org</a></li>
 </ul><a href="http://www.ndrn.org"></a>
+
+
+
+<h2 class="h4 resource-heading" style="">Sexual Abuse</h2>
+
+<ul class="ifvcc-resources-list">
+	<li>Illinois Coalition Against Sexual Assault
+	<br><a href="http://www.icasa.org">www.icasa.org</a> </li>
+	<li>End Violence Against Women International
+	<br><a href="http://Evawintl.org " target="_blank">Evawintl.org</a> </li>
+	<li>National Center on Domestic and Sexual Violence
+	<br><a href="http://www.ncdsv.org">www.ncdsv.org</a> </li>
+	<li>National Sexual Violence Resource Center <br>
+	<a href="http://www.nsvrc.org">www.nsvrc.org</a> </li>
+	<li>RAINN
+	<br><a href="https://rainn.org/">https://rainn.org/</a></li>
+</ul>
+
+
+
+<h2 class="h4 resource-heading" style="">Child Abuse</h2>
+
+<ul class="ifvcc-resources-list">
+	<li>Children's Advocacy Centers of Illinois
+	<br><a href="http://www.cacionline.org">www.cacionline.org</a> </li>
+	<li>Illinois Department of Children & Family Services <br>
+	<a href="http://www.state.il.us/dcfs/index/shtml">www.state.il.us/dcfs/index/shtml</a> </li>
+	<li>Prevent Child Abuse Illinois
+	<br><a href="http://www.preventchildabuseillinois.org">www.preventchildabuseillinois.org</a></li>
+</ul>
+
+
+
+<h2 class="h4 resource-heading" style="">Other Resources</h2>
+<ul class="ifvcc-resources-list">
+	<li>DHS: Illinois Partner Abuse Intervention Program (PAIP) Protocol Programs
+	<br>	<a href="https://www.dhs.state.il.us/page.aspx?item=33888">https://www.dhs.state.il.us/page.aspx?item=33888</a> </li>
+	<li>Illinois Automated Victim Notification
+	<br>	<a href="https://www.vinelink.com/vinelink/siteInfoAction.do?siteId=14004">https://www.vinelink.com/vinelink/siteInfoAction.d...</a> </li>
+	<li>Illinois Department of Human Services <br>
+	<a href="http://www.dhs.state.il.us">www.dhs.state.il.us</a> </li>
+	<li>Illinois Department of Public Health <br>
+	<a href="http://www.idph.state.il.us">www.idph.state.il.us</a> </li>
+	<li>Illinois State Police
+	<br>	<a href="http://www.isp.state.il.us/">http://www.isp.state.il.us/</a> </li>
+	<li>Office of the Illinois Attorney General
+	<br>	<a href="http://www.ag.state.il.us">www.ag.state.il.us</a> </li>
+	<li>Office for Victims of Crime
+	<br>	<a href="http://www.ovc.gov">www.ovc.gov</a> </li>
+	<li>Office on Violence Against Women
+	<br>	<a href="http://www.justice.gov/ovw">http://www.justice.gov/ovw</a> </li>
+	<li>Vera Institute of Justice
+	<br>	<a href="http://www.vera.org">www.vera.org</a></li>
+	<li>DHS Protocol for Approved Partner Abuse Intervention Programs<br>
+	The Department of Human Services funds and/or monitors Partner Abuse Intervention Programs across the state. These programs provide domestic violence perpetrator services such as assessment, individual and group education, and case coordination with referral sources. These services help perpetrators accept responsibility, modify abusive attitudes and beliefs, and give them tools to become and remain, healthy non-abusive partners and parents. All protocol approved programs must adhere to the standards established in Administrative Rule 89, Section 501.<br>
+	The following is a statewide list of programs that comply with the Illinois Protocol for Partner Abuse Intervention Programs. Also included is information on our funded domestic violence victim programs. The listing is organized by judicial districts. Consult the enclosed map to identify the judicial district which serves your county and correlates to the program service area. Sections of the list can be printed by highlighting the desired text, selecting print, selection. <br>
+	Please contact the Bureau of Domestic and Sexual Violence Prevention at 217-557-8306, if a different version is required or additional information is needed. Inquiries about protocol compliance may be directed to Teresa Tudor at 217-558-6192. <br>
+  Download: <a href="/assets/ifvcc/IDHS PAIP judicial listing Dec 2012.pdf">PAIP Listing - December 2012</a>
+	</li>
+</ul><a href="http://www.vera.org"></a>
+
+
 
 
 
