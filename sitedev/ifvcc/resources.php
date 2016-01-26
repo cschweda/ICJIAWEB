79934edfdb49a503816fd2e642d14b85d19d5658
cschweda <cschweda@gmail.com>
2016-01-26 13:29:53 -0600

    linked contact page names

--- sitedev/ifvcc/resources.php
+++ sitedev/ifvcc/resources.php
@@ -5,7 +5,12 @@
 
     <?php include 'partials/head.php';?>
 
-
+    <style>
+    ul.ifvcc-resources-list li {margin-bottom: 15px}
+    .resource-heading {margin-top: 40px; font-weight: 700; text-transform: uppercase; border-bottom: 1px solid #eee; padding-bottom: 8px}
+    .resource-heading.first {margin-top: 0}
+    .border-left {border-left: 1px solid #eee;}
+    </style>
 
 </head>
 
@@ -44,7 +49,7 @@
 
   <div class="container">
     <div class="col-md-2"></div>
-    <div class="col-md-9" style="border-left: 1px solid #eee;">
+    <div class="col-md-9 border-left" style="">
 
 
 
@@ -57,7 +62,48 @@
 
       <div class="resources-content">
 
-      
+<h2 class="h4 resource-heading first" >Domestic Violence</h2>
+
+<ul class="ifvcc-resources-list">
+	<li>Battered Women's Justice Project
+	<br><a href="http://www.Bwjp.org">www.Bwjp.org</a> </li>
+	<li>Corporate Alliance to End Partner Abuse
+	<br><a href="http://www.caepv.org">www.caepv.org</a> </li>
+	<li>End Violence Against Women International
+	<br>Evawintl.org </li>
+	<li>Illinois Coalition Against Domestic Violence
+	<br><a href="http://www.ilcadv.org">www.ilcadv.org</a></li>
+	<li>National Center on Domestic and Sexual Violence
+	<br><a href="http://www.ncdsv.org">www.ncdsv.org</a> </li>
+	<li>National Coalition Against Domestic Violence
+	<br><a href="http://www.ncadv.org">www.ncadv.org</a> </li>
+	<li>National Domestic Violence Hotline
+	<br>1-800-799-7233 </li>
+	<li>Stalking Resource Center <br>
+	<a href="http://www.victimsofcrime.org/our-programs/stalking-resource-center">http://www.victimsofcrime.org/our-programs/stalkin...</a></li>
+</ul>
+
+
+<h2 class="h4 resource-heading" style="">Elder Abuse</h2>
+
+<ul>
+	<li>National Center on Elder Abuse
+	<br><a href="http://www.ncea.aoa.gov">www.ncea.aoa.gov</a> </li>
+	<li>Illinois Department on Aging <br>
+	<a href="http://www.state.il.us/aging/">www.state.il.us/aging/</a></li>
+</ul>
+
+
+<h2 class="h4 resource-heading" style="">People with Disabilities</h2>
+
+<ul>
+	<li>Illinois Images
+	<br><a href="http://Illinoisimages.org" target="_blank">Illinoisimages.org</a> </li>
+	<li>National Center on Criminal Justice and Disability
+	<br><a href="http://www.thearc.org/nccjd">www.thearc.org/nccjd</a> </li>
+	<li>National Disability Rights Network
+	<br><a href="http://www.ndrn.org">www.ndrn.org</a></li>
+</ul><a href="http://www.ndrn.org"></a>
 
 
 
