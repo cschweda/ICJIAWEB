72ad47ee652caacfb6fb0241f61264d456b34304
cschweda <cschweda@gmail.com>
2016-04-14 07:39:17 -0500

    reversed sections mvtpc

--- _themes/icjia/js/mvtpc/mvtpc-app.js
+++ _themes/icjia/js/mvtpc/mvtpc-app.js
@@ -19,7 +19,7 @@ $(document).ready(function() {
     $('a.page-scroll').bind('click', function(event) {
       var $anchor = $(this);
       $('html, body').stop().animate({
-        scrollTop: $($anchor.attr('href')).offset().top - 50
+        scrollTop: $($anchor.attr('href')).offset().top - 70
       }, 1500, 'easeInOutExpo');
 
       event.preventDefault();
