6f276cc7f01fb829642b4b152075a16c8196a197
cschweda <cschweda@gmail.com>
2016-08-17 15:02:05 -0500

    updated graph

---
+++ _themes/icjia/js/highcharts/2016-08-17-drug-trends-and-distribution-in-illinois-a-survey-of-drug-task-forces.js
@@ -0,0 +1,61 @@
+function TableFunction() {
+  $('#hc-table1').toggle();
+}
+
+
+$(function() {
+
+  $('#hc-table1').hide();
+  $('#hc-fig1').highcharts({
+    chart: {
+      type: 'column'
+    },
+    title: {
+      text: 'Extent of illegal drug distribution in drug task force jurisdictions (n=19)'
+    },
+    xAxis: {
+      categories: ['Cannabis',	'Heroin',	'Prescription drugs',	'Cocaine',	'Crack cocaine']
+},
+    yAxis: {
+      min: 0,
+      title: {
+        text: ''
+      },
+    },
+    tooltip: {
+      formatter: function() {
+        return this.x + '</b>: <b>' + this.y + '</b>%<br>' + this.series.name;
+      }
+    },
+            series: [
+            {
+        		name: 'Serious problem',
+            data: [.58,.95,.47,.53,.47]
+        },
+        {
+            name: 'Moderate problem',
+            data: [.37,.05,.37,.37,.37]
+        },
+        {
+            name: 'Neutral',
+            data: [.05,0,.05,0,0]
+        },
+            {
+            name: 'Minor problem',
+            data: [0,0,.11,.11,.16]
+        },
+        ],
+    plotOptions: {
+      column: {
+        stacking: 'percent',
+        dataLabels: {
+          enabled: true,
+          color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white',
+          style: {
+            textShadow: '0 0 3px black'
+          }
+        }
+      }
+    },
+  });
+})();
