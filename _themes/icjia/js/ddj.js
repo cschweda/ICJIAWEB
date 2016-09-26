7a6c8cd4b519dee893d3b5ee03ebacf0dc39a91f
cschweda <cschweda@gmail.com>
2016-09-26 09:30:06 -0500

    new article BAM

--- _themes/icjia/js/ddj.js
+++ _themes/icjia/js/ddj.js
@@ -1,2 +1,45 @@
-!function(a){a(document).ready(function(){a(function(){a("a.page-scroll").bind("click",function(b){var c=a(this);a("html, body").stop().animate({scrollTop:a(c.attr("href")).offset().top-70},1500,"easeInOutExpo"),b.preventDefault()})}),a(function(){a(window).scroll(function(){a(this).scrollTop()>150?(a(".navbar-fixed-top").addClass("top-nav-collapse"),a("#navContainer").removeClass("container"),a(".offCanvasButton").show(),a(".navbar-default").css({background:"#fff"}),a(".navbar-default .navbar-nav > .active > a").css({"background-color":"#aa3c3e",color:"#fff"}),a(".navbar").fadeIn()):a(".navbar").fadeOut()})}),a("a.back-to-top").click(function(){return a("body, html").animate({scrollTop:0},1200),!1})})}(jQuery);
-//# sourceMappingURL=ddj.js.map
+(function($) {
+    $(document).ready(function() {
+        //jQuery for page scrolling feature - requires jQuery Easing plugin
+        $(function() {
+            $('a.page-scroll').bind('click', function(event) {
+                var $anchor = $(this);
+                $('html, body').stop().animate({
+                    scrollTop: $($anchor.attr('href')).offset().top - 70
+                }, 1500, 'easeInOutExpo');
+                event.preventDefault();
+            });
+        });
+        // hide .navbar first
+        //$(".navbar").hide();
+        // fade in .navbar
+        $(function() {
+            $(window).scroll(function() {
+                // set distance user needs to scroll before we fadeIn navbar
+                if ($(this).scrollTop() > 150) {
+                    $(".navbar-fixed-top").addClass("top-nav-collapse");
+                    $("#navContainer").removeClass("container");
+                    // $(".navbar-collapse > ul").removeClass("pull-right");
+                    //$(".top-bar").fadeOut(100);
+                    $(".offCanvasButton").show();
+                    $('.navbar-default').css({
+                        "background": "#fff"
+                    });
+                    $(".navbar-default .navbar-nav > .active > a").css({
+                        "background-color": "#aa3c3e",
+                        "color": "#fff"
+                    });
+                    $('.navbar').fadeIn();
+                } else {
+                    $('.navbar').fadeOut();
+                }
+            });
+        });
+        $('a.back-to-top').click(function() {
+            $('body, html').animate({
+                scrollTop: 0
+            }, 1200);
+            return false;
+        });
+    });
+}(jQuery));
