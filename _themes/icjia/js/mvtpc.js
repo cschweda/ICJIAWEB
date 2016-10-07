613f91f310dc518da35a65d380cffead0b25ea24
cschweda <cschweda@gmail.com>
2016-10-07 13:06:45 -0500

    DDJ u[date

--- _themes/icjia/js/mvtpc.js
+++ _themes/icjia/js/mvtpc.js
@@ -1,2 +1,214 @@
-$(document).ready(function(){var a=$(window).width();$(window).height();480>=a&&$("html, body").animate({scrollTop:$(".intro-message").offset().top-50},"fast"),$(function(){$("a.page-scroll").bind("click",function(a){var b=$(this);$("html, body").stop().animate({scrollTop:$(b.attr("href")).offset().top-70},1500,"easeInOutExpo"),a.preventDefault()})}),$(window).scroll(function(){$(".navbar").offset().top>50?($(".navbar-fixed-top").addClass("top-nav-collapse"),$("#navContainer").removeClass("container"),$(".top-bar").fadeOut(100),$(".offCanvasButton").show(),$(".navbar-default .navbar-nav > .active > a").css({"background-color":"#aa3c3e",color:"#fff"})):($(".navbar-fixed-top").removeClass("top-nav-collapse"),$("#navContainer").addClass("container"),$(".navbar-default .navbar-nav > .active > a").css({"background-color":"#fff",color:"#222"}),$(".top-bar").fadeIn(100),$(".offCanvasButton").hide())}),$(".bxslider").show().bxSlider({minSlides:1,preloadImages:"all",maxSlides:1,controls:!0,pager:!1,easing:"ease-in",auto:!1,adaptiveHeight:!0,nextSelector:"#slider-next",prevSelector:"#slider-prev",nextText:'<i class="fa fa-arrow-circle-right fa-2x"></i>',prevText:'<i class="fa fa-arrow-circle-left fa-2x"></i>'});var b=4e3;$("#counter1").countTo({from:0,to:36710,speed:b,refreshInterval:20,formatter:function(a,b){return a.toFixed(b.decimals).replace(/\B(?=(?:\d{3})+(?!\d))/g,",")},onComplete:function(a){$(this).addClass("countup-finished"),$(this).parent().parent().addClass("complete")}}),$("#counter2").countTo({from:0,to:68159,speed:b,refreshInterval:20,formatter:function(a,b){return a.toFixed(b.decimals).replace(/\B(?=(?:\d{3})+(?!\d))/g,",")},onComplete:function(a){$(this).addClass("countup-finished"),$(this).parent().parent().addClass("complete")}}),$("#counter3").countTo({from:0,to:14373,speed:b,refreshInterval:20,formatter:function(a,b){return a.toFixed(b.decimals).replace(/\B(?=(?:\d{3})+(?!\d))/g,",")},onComplete:function(a){$(this).addClass("countup-finished"),$(this).parent().parent().addClass("complete")}}),$("#counter4").countTo({from:0,to:17641,speed:b,refreshInterval:20,formatter:function(a,b){return a.toFixed(b.decimals).replace(/\B(?=(?:\d{3})+(?!\d))/g,",")},onComplete:function(a){$(this).addClass("countup-finished"),$(this).parent().parent().addClass("complete")}}),$("#counter5").countTo({from:0,to:7072,speed:b,refreshInterval:20,formatter:function(a,b){return a.toFixed(b.decimals).replace(/\B(?=(?:\d{3})+(?!\d))/g,",")},onComplete:function(a){$(this).addClass("countup-finished"),$(this).parent().parent().addClass("complete")}}),$("#counter6").countTo({from:0,to:41217,speed:b,refreshInterval:20,formatter:function(a,b){return a.toFixed(b.decimals).replace(/\B(?=(?:\d{3})+(?!\d))/g,",")},onComplete:function(a){$(this).addClass("countup-finished"),$(this).parent().parent().addClass("complete")}}),$("body").prepend('<a href="javascript:void(0);" class="back-to-top" style="display: none">Back to Top</a>');var c=550,d=120;$(window).scroll(function(){$(window).scrollTop()>c?$("a.back-to-top").fadeIn("fast"):$("a.back-to-top").fadeOut("fast"),$(window).scrollTop()>d?$(".prepbootstrap-sharing-container").fadeIn("slow"):$(".prepbootstrap-sharing-container").fadeOut("slow")}),$("a.back-to-top").click(function(){return $("body, html").animate({scrollTop:0},1200),!1})});
-//# sourceMappingURL=mvtpc.js.map
+$(document).ready(function() {
+
+
+
+  //bump window down 50px if on iPhone
+  var width = $(window).width(),
+    height = $(window).height();
+  if (width <= 480) {
+    $('html, body').animate({
+      scrollTop: $('.intro-message').offset().top - 50
+    }, 'fast');
+
+  }
+
+
+
+  //jQuery for page scrolling feature - requires jQuery Easing plugin
+  $(function() {
+    $('a.page-scroll').bind('click', function(event) {
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top - 70
+      }, 1500, 'easeInOutExpo');
+
+      event.preventDefault();
+    });
+  });
+
+
+
+  //jQuery to collapse the navbar on scroll
+  $(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+      $(".navbar-fixed-top").addClass("top-nav-collapse");
+      $("#navContainer").removeClass("container");
+      // $(".navbar-collapse > ul").removeClass("pull-right");
+
+      $(".top-bar").fadeOut(100);
+      $(".offCanvasButton").show();
+
+      $(".navbar-default .navbar-nav > .active > a").css({
+        "background-color": "#aa3c3e",
+        "color": "#fff"
+      });
+
+
+
+    } else {
+      $(".navbar-fixed-top").removeClass("top-nav-collapse");
+
+      $("#navContainer").addClass("container");
+      //$(".navbar-collapse > ul").addClass("pull-right");
+      $(".navbar-default .navbar-nav > .active > a").css({
+        "background-color": "#fff",
+        "color": "#222"
+      });
+      $(".top-bar").fadeIn(100);
+      $(".offCanvasButton").hide();
+
+    }
+  });
+
+
+  // slider for MVTPC info
+  $('.bxslider').show().bxSlider({
+    minSlides: 1,
+    preloadImages: 'all',
+    maxSlides: 1,
+    controls: true,
+    pager: false,
+    easing: 'ease-in',
+    auto: false,
+    adaptiveHeight: true,
+    nextSelector: '#slider-next',
+    prevSelector: '#slider-prev',
+    nextText: '<i class="fa fa-arrow-circle-right fa-2x"></i>',
+    prevText: '<i class="fa fa-arrow-circle-left fa-2x"></i>'
+  });
+
+
+
+  //jquery-countTo
+  var counterSpeed = 4000;
+
+
+  $('#counter1').countTo({
+    from: 0,
+    to: 36710,
+    speed: counterSpeed,
+    refreshInterval: 20,
+    formatter: function(value, options) {
+      return value.toFixed(options.decimals).replace(
+        /\B(?=(?:\d{3})+(?!\d))/g, ',');
+    },
+    onComplete: function(value) {
+      $(this).addClass('countup-finished');
+      $(this).parent().parent().addClass('complete');
+    }
+  });
+
+
+  $('#counter2').countTo({
+    from: 0,
+    to: 68159,
+    speed: counterSpeed,
+    refreshInterval: 20,
+    formatter: function(value, options) {
+      return value.toFixed(options.decimals).replace(
+        /\B(?=(?:\d{3})+(?!\d))/g, ',');
+    },
+    onComplete: function(value) {
+      $(this).addClass('countup-finished');
+      $(this).parent().parent().addClass('complete');
+    }
+  });
+
+
+  $('#counter3').countTo({
+    from: 0,
+    to: 14373,
+    speed: counterSpeed,
+    refreshInterval: 20,
+    formatter: function(value, options) {
+      return value.toFixed(options.decimals).replace(
+        /\B(?=(?:\d{3})+(?!\d))/g, ',');
+    },
+    onComplete: function(value) {
+      $(this).addClass('countup-finished');
+      $(this).parent().parent().addClass('complete');
+    }
+  });
+
+
+  $('#counter4').countTo({
+    from: 0,
+    to: 17641,
+    speed: counterSpeed,
+    refreshInterval: 20,
+    formatter: function(value, options) {
+      return value.toFixed(options.decimals).replace(
+        /\B(?=(?:\d{3})+(?!\d))/g, ',');
+    },
+    onComplete: function(value) {
+      $(this).addClass('countup-finished');
+      $(this).parent().parent().addClass('complete');
+    }
+  });
+
+
+  $('#counter5').countTo({
+    from: 0,
+    to: 7072,
+    speed: counterSpeed,
+    refreshInterval: 20,
+    formatter: function(value, options) {
+      return value.toFixed(options.decimals).replace(
+        /\B(?=(?:\d{3})+(?!\d))/g, ',');
+    },
+    onComplete: function(value) {
+      $(this).addClass('countup-finished');
+      $(this).parent().parent().addClass('complete');
+    }
+  });
+
+
+  $('#counter6').countTo({
+    from: 0,
+    to: 41217,
+    speed: counterSpeed,
+    refreshInterval: 20,
+    formatter: function(value, options) {
+      return value.toFixed(options.decimals).replace(
+        /\B(?=(?:\d{3})+(?!\d))/g, ',');
+    },
+    onComplete: function(value) {
+      $(this).addClass('countup-finished');
+      $(this).parent().parent().addClass('complete');
+    }
+  });
+
+  $('body').prepend(
+    '<a href="javascript:void(0);" class="back-to-top" style="display: none">Back to Top</a>'
+  );
+
+  var amountScrolled = 550;
+
+  var socialbar = 120;
+
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > amountScrolled) {
+      $('a.back-to-top').fadeIn('fast');
+    } else {
+      $('a.back-to-top').fadeOut('fast');
+    }
+
+    if ($(window).scrollTop() > socialbar) {
+      $('.prepbootstrap-sharing-container').fadeIn('slow');
+    } else {
+      $('.prepbootstrap-sharing-container').fadeOut('slow');
+    }
+
+
+  });
+
+  $('a.back-to-top').click(function() {
+    $('body, html').animate({
+      scrollTop: 0
+    }, 1200);
+    return false;
+  });
+
+
+
+});
--- _themes/icjia/js/tvpp.js
+++ _themes/icjia/js/tvpp.js
@@ -1,2 +1,278 @@
-function progressBarInit(){var a=function(){return $(document).height()-$(window).height()},b=function(){return $(window).scrollTop()};if("max"in document.createElement("progress")){var c=$("progress");c.attr({max:a()}),$(document).on("scroll",function(){c.attr({value:b()})}),$(window).resize(function(){c.attr({max:a(),value:b()})})}else{var d,e,c=$(".progress-bar"),f=a(),g=function(){return d=b(),e=d/f*100,e+="%"},h=function(){c.css({width:g()})};$(document).on("scroll",h),$(window).on("resize",function(){f=a(),h()})}}$(document).ready(function(){var a=$(window).width();$(window).height();480>=a&&$("html, body").animate({scrollTop:$(".intro-message").offset().top-50},"fast"),$(function(){$("a.page-scroll").bind("click",function(a){var b=$(this);$("html, body").stop().animate({scrollTop:$(b.attr("href")).offset().top-70},1500,"easeInOutExpo"),a.preventDefault()})}),$(window).scroll(function(){$(".navbar").offset().top>40?($(".navbar-fixed-top").addClass("top-nav-collapse"),$("#navContainer").removeClass("container"),$(".top-bar").fadeOut(100),$(".offCanvasButton").show(),$(".navbar-brand-tvpp").css({"font-size":"14px"}),$(".navbar-brand-illinois").css({"font-size":"14px","margin-left":"0px"})):($(".navbar-fixed-top").removeClass("top-nav-collapse"),$("#navContainer").addClass("container"),$(".top-bar").fadeIn(100),$(".offCanvasButton").hide(),$(".navbar-brand-tvpp").css({"font-size":"18px"}),$(".navbar-brand-illinois").css({"font-size":"18px","margin-left":"-20px"}))}),$("body").prepend('<a href="javascript:void(0);" class="back-to-top" style="display: none">Back to Top</a>');var b=550,c=120;$(window).scroll(function(){$(window).scrollTop()>b?$("a.back-to-top").fadeIn("fast"):$("a.back-to-top").fadeOut("fast"),$(window).scrollTop()>c?$(".prepbootstrap-sharing-container").fadeIn("slow"):$(".prepbootstrap-sharing-container").fadeOut("slow")}),$("a.back-to-top").click(function(){return $("body, html").animate({scrollTop:0},1200),!1})}),$(document).ready(function(){progressBarInit()}),$(document).ready(function(){$("#flat").addClass("active"),$("#progressBar").addClass("flat"),$("#flat").on("click",function(){$("#progressBar").removeClass().addClass("flat"),$("a").removeClass(),$(this).addClass("active"),$(this).preventDefault()}),$("#single").on("click",function(){$("#progressBar").removeClass().addClass("single"),$("a").removeClass(),$(this).addClass("active"),$(this).preventDefault()}),$("#multiple").on("click",function(){$("#progressBar").removeClass().addClass("multiple"),$("a").removeClass(),$(this).addClass("active"),$(this).preventDefault()}),$("#semantic").on("click",function(){$("#progressBar").removeClass().addClass("semantic"),$("a").removeClass(),$(this).addClass("active"),$(this).preventDefault(),alert("hello")}),$(document).on("scroll",function(){maxAttr=$("#progressBar").attr("max"),valueAttr=$("#progressBar").attr("value"),percentage=valueAttr/maxAttr*100})});
-//# sourceMappingURL=tvpp.js.map
+$(document).ready(function() {
+
+  // TVPP JS
+
+
+  //bump window down 50px if on iPhone
+  var width = $(window).width(),
+    height = $(window).height();
+  if (width <= 480) {
+    $('html, body').animate({
+      scrollTop: $('.intro-message').offset().top - 50
+    }, 'fast');
+
+  }
+
+
+
+  //jQuery for page scrolling feature - requires jQuery Easing plugin
+  $(function() {
+    $('a.page-scroll').bind('click', function(event) {
+      var $anchor = $(this);
+      $('html, body').stop().animate({
+        scrollTop: $($anchor.attr('href')).offset().top - 70
+      }, 1500, 'easeInOutExpo');
+
+      event.preventDefault();
+    });
+  });
+
+
+
+  //jQuery to collapse the navbar on scroll
+  $(window).scroll(function() {
+    if ($(".navbar").offset().top > 40) {
+      $(".navbar-fixed-top").addClass("top-nav-collapse");
+      $("#navContainer").removeClass("container");
+      // $(".navbar-collapse > ul").removeClass("pull-right");
+
+      $(".top-bar").fadeOut(100);
+      $(".offCanvasButton").show();
+
+      // $(".navbar-default .navbar-nav > .active > a").css({
+      //
+      //   "color": "#e28642"
+      // });
+
+      $(".navbar-brand-tvpp").css({
+        "font-size": "14px"
+      });
+
+      $(".navbar-brand-illinois").css({
+        "font-size": "14px",
+        "margin-left": "0px"
+      });
+
+
+    } else {
+      $(".navbar-fixed-top").removeClass("top-nav-collapse");
+
+      $("#navContainer").addClass("container");
+      //$(".navbar-collapse > ul").addClass("pull-right");
+      // $(".navbar-default .navbar-nav > .active > a").css({
+      //
+      //   "color": "#e28642"
+      // });
+      $(".top-bar").fadeIn(100);
+      $(".offCanvasButton").hide();
+
+      $(".navbar-brand-tvpp").css({
+        "font-size": "18px"
+      });
+
+      $(".navbar-brand-illinois").css({
+        "font-size": "18px",
+        "margin-left": "-20px"
+      });
+
+    }
+  });
+
+
+  // slider for MVTPC info
+  // $('.bxslider').show().bxSlider({
+  //   minSlides: 1,
+  //   preloadImages: 'all',
+  //   maxSlides: 1,
+  //   controls: true,
+  //   pager: false,
+  //   easing: 'ease-in',
+  //   auto: false,
+  //   adaptiveHeight: true,
+  //   nextSelector: '#slider-next',
+  //   prevSelector: '#slider-prev',
+  //   nextText: '<i class="fa fa-arrow-circle-right fa-2x"></i>',
+  //   prevText: '<i class="fa fa-arrow-circle-left fa-2x"></i>'
+  // });
+
+
+
+  $('body').prepend(
+    '<a href="javascript:void(0);" class="back-to-top" style="display: none">Back to Top</a>'
+  );
+
+  var amountScrolled = 550;
+
+  var socialbar = 120;
+
+  $(window).scroll(function() {
+    if ($(window).scrollTop() > amountScrolled) {
+      $('a.back-to-top').fadeIn('fast');
+    } else {
+      $('a.back-to-top').fadeOut('fast');
+    }
+
+    if ($(window).scrollTop() > socialbar) {
+      $('.prepbootstrap-sharing-container').fadeIn('slow');
+    } else {
+      $('.prepbootstrap-sharing-container').fadeOut('slow');
+    }
+
+
+  });
+
+  $('a.back-to-top').click(function() {
+    $('body, html').animate({
+      scrollTop: 0
+    }, 1200);
+    return false;
+  });
+
+
+
+});
+
+
+
+$(document).ready(function() {
+
+
+  progressBarInit();
+
+
+
+});
+
+
+$(document).ready(function() {
+
+  $('#flat').addClass("active");
+  $('#progressBar').addClass('flat');
+
+  $('#flat').on('click', function() {
+    $('#progressBar').removeClass().addClass('flat');
+    $('a').removeClass();
+    $(this).addClass('active');
+    $(this).preventDefault();
+  });
+
+  $('#single').on('click', function() {
+    $('#progressBar').removeClass().addClass('single');
+    $('a').removeClass();
+    $(this).addClass('active');
+    $(this).preventDefault();
+  });
+
+  $('#multiple').on('click', function() {
+    $('#progressBar').removeClass().addClass('multiple');
+    $('a').removeClass();
+    $(this).addClass('active');
+    $(this).preventDefault();
+  });
+
+  $('#semantic').on('click', function() {
+    $('#progressBar').removeClass().addClass('semantic');
+    $('a').removeClass();
+    $(this).addClass('active');
+    $(this).preventDefault();
+    alert('hello');
+  });
+
+  $(document).on('scroll', function() {
+
+    maxAttr = $('#progressBar').attr('max');
+    valueAttr = $('#progressBar').attr('value');
+    percentage = (valueAttr / maxAttr) * 100;
+
+    // if (percentage < 49) {
+    // 	document.styleSheets[0].addRule('.semantic', 'color: red');
+    // 	document.styleSheets[0].addRule(
+    // 		'.semantic::-webkit-progress-value',
+    // 		'background-color: red');
+    // 	document.styleSheets[0].addRule('.semantic::-moz-progress-bar',
+    // 		'background-color: red');
+    // } else if (percentage < 98) {
+    // 	document.styleSheets[0].addRule('.semantic', 'color: orange');
+    // 	document.styleSheets[0].addRule(
+    // 		'.semantic::-webkit-progress-value',
+    // 		'background-color: orange');
+    // 	document.styleSheets[0].addRule('.semantic::-moz-progress-bar',
+    // 		'background-color: orange');
+    // } else {
+    // 	document.styleSheets[0].addRule('.semantic', 'color: green');
+    // 	document.styleSheets[0].addRule(
+    // 		'.semantic::-webkit-progress-value',
+    // 		'background-color: green');
+    // 	document.styleSheets[0].addRule('.semantic::-moz-progress-bar',
+    // 		'background-color: green');
+    // }
+  });
+
+});
+
+
+/*
+Function to (re)initialize the progress bar.
+This is called if elements on the page are expanded (e.g., the bios on the 'Commission' page)
+*/
+
+
+function progressBarInit() {
+  var getMax = function() {
+    return $(document).height() - $(window).height();
+  }
+
+  var getValue = function() {
+    return $(window).scrollTop();
+  }
+
+  if ('max' in document.createElement('progress')) {
+    // Browser supports progress element
+    var progressBar = $('progress');
+
+    // Set the Max attr for the first time
+    progressBar.attr({
+      max: getMax()
+    });
+
+    $(document).on('scroll', function() {
+      // On scroll only Value attr needs to be calculated
+      progressBar.attr({
+        value: getValue()
+      });
+    });
+
+    $(window).resize(function() {
+      // On resize, both Max/Value attr needs to be calculated
+      progressBar.attr({
+        max: getMax(),
+        value: getValue()
+      });
+    });
+  } else {
+    var progressBar = $('.progress-bar'),
+      max = getMax(),
+      value, width;
+
+    var getWidth = function() {
+      // Calculate width in percentage
+      value = getValue();
+      width = (value / max) * 100;
+      width = width + '%';
+      return width;
+    }
+
+    var setWidth = function() {
+      progressBar.css({
+        width: getWidth()
+      });
+    }
+
+    $(document).on('scroll', setWidth);
+    $(window).on('resize', function() {
+      // Need to reset the Max attr
+      max = getMax();
+      setWidth();
+    });
+  }
+}
