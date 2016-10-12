1a114d8de014188335bed87b8fed0c27e7421379
cschweda <cschweda@gmail.com>
2016-10-12 15:18:20 -0500

    updated infographic to include grunt

--- sitedev/infographic2/gruntfile.js
+++ sitedev/infographic2/gruntfile.js
@@ -2,22 +2,50 @@
 
 module.exports = function(grunt) {
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
+
+	pkg: grunt.file.readJSON('package.json'),
 		sass: {
 			dist: {
+				options: {                       // Target options
+			 		sourcemap: 'inline',
+					style: 'compressed'
+		 		},
 				files: {
-					'css/app.css' : 'scss/style.scss'
+					'./dist/css/app.css' : './src/scss/style.scss'
 				}
 			}
 		},
+
 		watch: {
 			css: {
-				files: '**/*.scss',
+				files: 'src/**/*.scss',
 				tasks: ['sass']
+			},
+			js: {
+				files: 'src/js/*.js',
+				tasks: ['uglify']
 			}
-		}
+		},
+
+
+	uglify: {
+  	options: {
+      mangle: false,
+			sourceMap: true,
+      sourceMapName: './dist/js/app.min.map'
+    },
+    dist: {
+      files: {
+        './dist/js/app.min.js': ['./src/js/*.js']
+      }
+    }
+  }
+
+
 	});
+
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.registerTask('default',['watch']);
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.registerTask('default',['sass','uglify','watch']);
 };
