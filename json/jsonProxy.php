a7ed325867db50643230a2edf4949ba5aa8f858f
cschweda <cschweda@gmail.com>
2016-10-28 08:50:18 -0500

    server updates

---
+++ json/jsonProxy.php
@@ -0,0 +1,11 @@
+<?php
+
+$url = "http://www.icjia.state.il.us/json/Counties.json";
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$result = curl_exec ($ch);
+curl_close ($ch);
+echo $result;
+
+?>
