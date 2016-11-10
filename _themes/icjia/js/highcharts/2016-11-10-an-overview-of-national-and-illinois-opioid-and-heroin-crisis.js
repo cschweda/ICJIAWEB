d01352cab8ce65d3b3d4b8e9da8875fed6cf5864
cschweda <cschweda@gmail.com>
2016-11-10 13:47:22 -0600

    highmapds

--- _themes/icjia/js/highcharts/2016-11-10-an-overview-of-national-and-illinois-opioid-and-heroin-crisis.js
+++ _themes/icjia/js/highcharts/2016-11-10-an-overview-of-national-and-illinois-opioid-and-heroin-crisis.js
@@ -1,5 +1,9 @@
+
+$('#table_view1').hide()
+$('#table1').hide()
+
 $(function() {
-  $('#container').highcharts({
+  $('#hc-fig1').highcharts({
 
     chart: {
       type: 'line'
@@ -18,6 +22,51 @@ $(function() {
     },
     xAxis: {
       categories: ['2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012']
+    },
+    yAxis: {
+      min: 0,
+      title: {
+        text: ''
+      },
+    },
+    tooltip: {
+      formatter: function() {
+        return 'The value for <b>' + this.x + '</b> is <b>' + this.y + '</b>, in series ' + this.series.name;
+      }
+    },
+
+    plotOptions: {
+      series: {
+        stacking: ''
+      }
+    },
+  });
+});
+
+$('#table_view2').hide()
+$('#table2').hide()
+
+
+$(function() {
+  $('#hc-fig2').highcharts({
+
+    chart: {
+      type: 'line'
+    },
+
+    data: {
+      table: 'table2'
+    },
+    title: {
+      text: 'Figure 2',
+      x: -20 //center
+    },
+    subtitle: {
+      text: 'National drug overdose deaths by drug, 1999-2014',
+      x: -20
+    },
+    xAxis: {
+      categories: ['2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012','2013','2014']
     },
     yAxis: {
       min: 0,
