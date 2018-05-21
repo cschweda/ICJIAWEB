72198851d7db2ce8d58e736cc32489ed7fffdbe2
cschweda <cschweda@gmail.com>
2018-05-21 14:23:20 -0500

    server update

--- _themes/icjia/js/highcharts/2018-05-09-pmp-article.js
+++ _themes/icjia/js/highcharts/2018-05-09-pmp-article.js
@@ -1,69 +1,70 @@
-$(function(){
-Highcharts.chart('hc-fig3', {
-Highcharts.setOptions({
-    lang: {
-        thousandsSep: ','
-    }
-});
+(function() {
 
-Highcharts.chart('container', {
+  Highcharts.setOptions({
+    lang: {
+      thousandsSep: ','
+    }
+  });
+
+  Highcharts.chart('hc-fig3', {
 
     title: {
-        text: null
+      text: null
     },
 
     subtitle: {
-        text: null
+      text: null
     },
 
     yAxis: {
-        title: {
-            text: null 
-            
-        }
+      title: {
+        text: null
+
+      }
     },
     legend: {
-        layout: 'vertical',
-        align: 'right',
-        verticalAlign: 'middle',
-			itemStyle: {
-      			width: "100px",
-            textOverflow: null
-    }},
+      layout: 'vertical',
+      align: 'right',
+      verticalAlign: 'middle',
+      itemStyle: {
+        width: "100px",
+        textOverflow: null
+      }
+    },
 
     plotOptions: {
-        series: {
-            label: {
-                connectorAllowed: false
-            },
-            pointStart: 2008
-        }
+      series: {
+        label: {
+          connectorAllowed: false
+        },
+        pointStart: 2008
+      }
     },
 
     series: [{
-        name: 'Total Opioid Prescriptions Filled',
-        data: [7255683, 7533330, 7711516, 7682165, 9027623, 8429711, 9087044, 8709276, 7584657]
+      name: 'Total Opioid Prescriptions Filled',
+      data: [7255683, 7533330, 7711516, 7682165, 9027623, 8429711, 9087044, 8709276, 7584657]
     }, {
-        name: 'Number of Individuals Filling at least 1 Opioid Prescription',
-        data: [2175878, 2197546, 2233229, 2172422, 2392930, 2267498, 2321555, 2363737, 1993870]
+      name: 'Number of Individuals Filling at least 1 Opioid Prescription',
+      data: [2175878, 2197546, 2233229, 2172422, 2392930, 2267498, 2321555, 2363737, 1993870]
     }],
 
     responsive: {
-        rules: [{
-            condition: {
-                maxWidth: 500
-            },
-            chartOptions: {
-                legend: {
-                    layout: 'horizontal',
-                    align: 'bottom',
-                    verticalAlign: 'bottom'
-                }
-            }
-        }]
+      rules: [{
+        condition: {
+          maxWidth: 500
+        },
+        chartOptions: {
+          legend: {
+            layout: 'horizontal',
+            align: 'bottom',
+            verticalAlign: 'bottom'
+          }
+        }
+      }]
     }
 
-})
+  })
 })();
 
 
