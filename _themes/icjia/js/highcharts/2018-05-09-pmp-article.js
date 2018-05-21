1d8f33b27cb33570880b8e42a8733f255fe1689b
cschweda <cschweda@gmail.com>
2018-05-21 11:45:57 -0500

    server update

--- _themes/icjia/js/highcharts/2018-05-09-pmp-article.js
+++ _themes/icjia/js/highcharts/2018-05-09-pmp-article.js
@@ -1,12 +1,13 @@
-$(function() {
-  $('#hc-fig3').highcharts({
-setOptions({
+$(function(){
+Highcharts.chart('hc-fig3', {
+Highcharts.setOptions({
     lang: {
-        thousandsSep: ''
+        thousandsSep: ','
     }
 });
 
-chart('container', {
+Highcharts.chart('container', {
+
     title: {
         text: null
     },
@@ -62,6 +63,313 @@ chart('container', {
         }]
     }
 
-     }]
-  });
+})
+})();
+
+
+$(function () {
+    Highcharts.chart('hc-fig4', {
+Highcharts.setOptions({
+    lang: {
+        thousandsSep: ','
+    }
 });
+
+Highcharts.chart('container', {
+
+    title: {
+        text: null
+    },
+
+    subtitle: {
+        text: null
+    },
+
+    yAxis: {
+        title: {
+            text: null 
+            
+        }
+    },
+    legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'middle',
+        itemStyle: {
+      			width: "100px",
+            textOverflow: null
+    		}
+    },
+
+    plotOptions: {
+        series: {
+            label: {
+                connectorAllowed: false
+            },
+            pointStart: 2008
+        }
+    },
+
+    series: [{
+        name: '3+ Prescribers OR 3+ Pharmacies',
+        data: [225632, 245326, 254454, 256768, 303021, 262931, 298690, 360042, 322957]
+    }, {
+        name: '3+ Prescribers AND 3+ Pharmacies',
+        data: [55394, 56539, 55335, 53493, 60743, 50369, 54592, 61285, 43618]
+    }],
+
+    responsive: {
+        rules: [{
+            condition: {
+                maxWidth: 500
+            },
+            chartOptions: {
+                legend: {
+                    layout: 'horizontal',
+                    align: 'bottom',
+                    verticalAlign: 'bottom'
+                }
+            }
+        }]
+    }
+
+})
+})();
+
+	
+$(function () {
+    Highcharts.chart('hc-fig5', {
+Highcharts.setOptions({
+    lang: {
+        thousandsSep: ','
+    }
+});
+
+Highcharts.chart('container', {
+
+    title: {
+        text: null
+    },
+
+    subtitle: {
+        text: null
+    },
+
+    yAxis: {
+        title: {
+            text: null 
+            
+        }
+    },
+    legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'middle',
+        itemStyle: {
+      			width: "100px",
+            textOverflow: null
+    		}
+    },
+
+    plotOptions: {
+        series: {
+            label: {
+                connectorAllowed: false
+            },
+            pointStart: 2008
+        }
+    },
+
+    series: [{
+        name: '6+ Prescribers OR 6+ Pharmacies',
+        data: [225632, 245326, 254454, 256768, 303021, 262931, 298690, 360042, 322957]
+    }, {
+        name: '6+ Prescribers AND 6+ Pharmacies',
+        data: [55394, 56539, 55335, 53493, 60743, 50369, 54592, 61285, 43618]
+    }],
+
+    responsive: {
+        rules: [{
+            condition: {
+                maxWidth: 500
+            },
+            chartOptions: {
+                legend: {
+                    layout: 'horizontal',
+                    align: 'bottom',
+                    verticalAlign: 'bottom'
+                }
+            }
+        }]
+    }
+
+})
+})();
+
+
+$(function () {
+    Highcharts.chart('hc-fig7', {
+Highcharts.setOptions({
+    lang: {
+        thousandsSep: ','
+    }
+});
+
+Highcharts.chart('container', {
+
+    title: {
+        text: null
+    },
+
+    subtitle: {
+        text: null
+    },
+
+    yAxis: {
+        title: {
+            text: null 
+            
+        }
+    },
+    legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'middle',
+        itemStyle: {
+      			width: "100px",
+            textOverflow: null
+    		}
+    },
+
+    plotOptions: {
+        series: {
+            label: {
+                connectorAllowed: false
+            },
+            pointStart: 2008
+        }
+    },
+
+    series: [{
+        name: 'Hydrocodone',
+        data: [4262828, 4569375, 4838110, 5283083, 5297637, 4951527, 5204414, 4501193, 3497895]
+    }, {
+        name: 'Other Opioids',
+        data: [1492267, 1456266, 1342137, 724012, 752087, 718307, 787236, 788935, 626317]
+    }, {
+        name: 'Tramadol',
+        data: [214, 270, 576, 21452, 1416567, 1368029, 1586497, 1742995, 1559501]
+    }, {
+        name: 'Codeine',
+        data: [860034, 847462, 812282, 833372, 738985, 621267, 656961, 751645, 658061]
+    }, {
+    		name: 'Oxycodone Short Acting',
+        data: [264449, 284700, 319906, 367960, 395556, 389218, 444580, 502746, 465476]
+    }, {
+    		name: 'Fentanyl Long Acting',
+        data: [259405, 262749, 260858, 274472, 266826, 229476, 249966, 252709, 212863]
+    }],
+
+    responsive: {
+        rules: [{
+            condition: {
+                maxWidth: 500
+            },
+            chartOptions: {
+                legend: {
+                    layout: 'horizontal',
+                    align: 'bottom',
+                    verticalAlign: 'bottom'
+                }
+            }
+        }]
+    }
+
+})
+})();
+
+
+$(function () {
+    Highcharts.chart('hc-fig8', {
+Highcharts.setOptions({
+    lang: {
+        thousandsSep: ','
+    }
+});
+
+Highcharts.chart('container', {
+
+    title: {
+        text: null
+    },
+
+    subtitle: {
+        text: null
+    },
+
+    yAxis: {
+        title: {
+            text: null 
+            
+        }
+    },
+    legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'middle',
+        itemStyle: {
+      			width: "100px",
+            textOverflow: null
+    		}
+        
+    },
+
+    plotOptions: {
+        series: {
+            label: {
+                connectorAllowed: false
+            },
+            pointStart: 2013
+        }
+    },
+
+    series: [{
+        name: 'Heroin',
+        data: [583, 711, 844, 1007]
+    }, {
+        name: 'Fentanyl',
+        data: [58, 92, 234, 606]
+    }, {
+        name: 'Any Natural/Semi-Synthetic Opioid',
+        data: [184, 251, 271, 370]
+    }, {
+        name: '4-ANPP',
+        data: [0, 0, 0, 219]
+    }, {
+    		name: 'Furanyl Fentanyl',
+        data: [0, 0, 0, 188]
+    }, {
+    		name: 'Methadone',
+        data: [92, 106, 99, 121]
+    }, {
+    		name: 'Tramadol',
+        data: [24, 29, 31, 56]
+    }],
+
+    responsive: {
+        rules: [{
+            condition: {
+                maxWidth: 500
+            },
+            chartOptions: {
+                legend: {
+                    layout: 'horizontal',
+                    align: 'bottom',
+                    verticalAlign: 'bottom'
+                }
+            }
+        }]
+    }
+
+})
+})();
